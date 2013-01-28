4c51c5701c351e80445b54a8c1ad5ebbad0a7330
bretthop <brett.hoppz@gmail.com>
2013-01-28 11:10:02 +1100

    * Replace jQuery Modal with Twitter Bootstrap Modal * Removed jQuery UI libs

--- static/resources/js/frontend.js
+++ static/resources/js/frontend.js
@@ -1,3 +1,16 @@
+/* Setup front end event handlers */
+var addProposalValidator = $("form").validate({
+    errorPlacement: function(error, element) {
+        //Override errorPlacement function (this stops the automatic 'This field is required' message from showing)
+        return true;
+    }
+});
+
+$('#addProposalModal').on('hide', function () {
+    addProposalValidator.resetForm();
+    $('#addProposalModal').find('input').val('');
+});
+
 //TODO: Move 'show/hide ajax loader' into the 'ajax' request, or make it generic so all requests (or request ques) show it
 function showAjaxLoader()
 {
@@ -11,7 +24,16 @@ function hideAjaxLoader()
 
 function showAddProposalModal()
 {
-    $(".addProposalForm").dialog("open");
+    $('#addProposalModal').modal();
+}
+
+function handleAddProposalClick()
+{
+    var success = addProposal();
+
+    if (success) {
+        $('#addProposalModal').modal('hide');
+    }
 }
 
 function resetAddProposalForm()
