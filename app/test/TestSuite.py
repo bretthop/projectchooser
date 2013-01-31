2dbdc42e2da7cf42fdfe0a63bf0c6ffb3e0dbb09
Trav3ll3r <sinisa.poznic@gmail.com>
2013-01-31 23:39:44 +1100

    * implemented unit test to test VoteService methods * TODO: make tests less dependable (the ProposalService and VoteService tests work only if BackerService unit tests are executed before them)

--- app/test/TestSuite.py
+++ app/test/TestSuite.py
@@ -8,24 +8,4 @@ from app.test.cases.TestVoteService import TestVoteService
 from app.test.fixtures.DBFixtures import DBFixtures
 
 class TestSuite(unittest2.TestSuite):
-
-    def load_tests(self, tests, pattern):
-        suite = unittest2.TestSuite()
-        case001 = self.loadTestsFromTestCase(DBFixtures)
-        suite.addTests(case001)
-
-        case101 = self.loadTestsFromTestCase(TestVoteTypeUtil)
-        suite.addTests(case101)
-
-        case110 = self.loadTestsFromTestCase(TestBackerService)
-        suite.addTests(case110)
-
-        case120 = self.loadTestsFromTestCase(TestProposalService)
-        suite.addTests(case120)
-
-#TODO: check why proposal.votes collection is empty after voting
-#        case130 = self.loadTestsFromTestCase(TestVoteService)
-#        suite.addTests(case130)
-
-
-        return suite
+    pass
--- app/test/cases/TestVoteService.py
+++ app/test/cases/TestVoteService.py
@@ -4,6 +4,7 @@ from google.appengine.ext import db
 
 from app.data.models import Proposal
 from app.data.enums.VoteTypeEnum import VoteTypeEnum
+from app.util.VoteTypeUtil import VoteTypeUtil
 
 from app.services.ProposalService import ProposalService
 from app.services.VoteService import VoteService
@@ -36,14 +37,37 @@ class TestVoteService(BaseUnitTest):
 
         self.getVoteService().VoteForProposal(_proposalId, _voteTypeLabel, self._backerEmail)
 
-        self.reportResult(message='voted')
-
         _proposal = Proposal.get_by_id(_proposalId)
 
         self.assertIsNotNone(_proposal, 'Proposal is None')
         self.assertTrue(_proposal.votes.count() > 0, 'Proposal has no votes')
 
-        if _proposal is not None and _proposal.votes.count() > 0:
+        if _proposal.votes.count() > 0:
             for v in _proposal.votes:
                 if v.voteType.label == VoteTypeEnum.GOLD:
                     self.reportResult(message='vote saved successfully')
+
+    def test_withdrawVote(self):
+        _proposalId = db.GqlQuery("SELECT __key__ FROM Proposal WHERE name = '" + self._testProposalName + "'").get().id()
+        _proposal = Proposal.get_by_id(_proposalId)
+
+        self.assertIsNotNone(_proposal, 'Proposal is None')
+        self.assertTrue(_proposal.votes.count() > 0, 'Proposal has no votes')
+
+        _voteId = db.GqlQuery("SELECT __key__ FROM Vote WHERE proposal = :pProposal AND userId = :pUserEmail AND voteType = :pVoteType",
+            pProposal  =_proposal,
+            pUserEmail = self._backerEmail,
+            pVoteType  = VoteTypeUtil.GetVoteTypeByLabel(VoteTypeEnum.GOLD)
+        ).get().id()
+
+        self.assertIsNotNone(_voteId, 'Vote is None')
+
+        self.getVoteService().WithdrawVote(_voteId, self._backerEmail)
+
+        #refetch proposal from DB
+        _proposal = Proposal.get_by_id(_proposalId)
+
+        self.assertTrue(_proposal.votes.count() == 0, 'Withdraw vote failed')
+
+        if _proposal.votes.count() == 0:
+            self.reportResult(message='vote removed successfully')
