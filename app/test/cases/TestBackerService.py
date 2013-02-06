4106d69dd1b724c221f0aa27e1176945e52ab86b
Trav3ll3r <sinisa.poznic@gmail.com>
2013-02-06 22:14:44 +1100

    * fixed up some unit tests (some are still broken due Domain being required property of Proposal)

--- app/test/cases/TestBackerService.py
+++ app/test/cases/TestBackerService.py
@@ -1,6 +1,7 @@
 from app.test.cases.BaseUnitTest import BaseUnitTest
 
 from app.services.BackerService import BackerService
+from app.data.models import Role
 
 class TestBackerService(BaseUnitTest):
 
@@ -15,8 +16,13 @@ class TestBackerService(BaseUnitTest):
 
     def test_getBackerByEmail(self):
         _backerEmail = 'test_backer_2013-01-29@project.chooser.com.au'
+        _backerUsername = 'test_backer_2013-01-29'
+        _backerPassword = 'password'
+        _backerRole = Role(name='BACKER').put()
 
-        _backer = self.getBackerService().GetBackerByEmail(_backerEmail)
+        _backer = self.getBackerService().CreateBacker(_backerEmail, _backerUsername, _backerPassword, _backerRole)
+
+        #_backer = self.getBackerService().GetBackerByEmail(_backerEmail)
 
         if _backer is not None:
             self.reportResult()
--- app/test/cases/TestProposalService.py
+++ app/test/cases/TestProposalService.py
@@ -3,10 +3,12 @@ from app.test.cases.BaseUnitTest import BaseUnitTest
 from app.services.ProposalService import ProposalService
 
 from app.data.models import Proposal
+from app.data.model.Domain import Domain
 
 class TestProposalService(BaseUnitTest):
 
     _proposalService = None
+    _testDomainName = 'TEST DOMAIN TITLE #1'
 
     #singleton pattern
     def getProposalService(self):
@@ -16,13 +18,19 @@ class TestProposalService(BaseUnitTest):
         return self._proposalService
 
     def test_createNewProposal(self):
+        _domain = Domain(
+            title = self._testDomainName,
+            description = 'TEST DOMAIN DESCRIPTION #1',
+        ).put()
+
         _testProposalName = 'TEST PROPOSAL #1'
 
         _proposal = Proposal(
             name = _testProposalName,
             description = 'DESCRIPTION OF ' + _testProposalName,
             technologiesUsed = 'TECHNOLOGIES USER IN ' + _testProposalName,
-            status = 'OPEN'
+            status = 'OPEN',
+            domain = _domain
         )
 
         result = self.getProposalService().saveProposal(_proposal)
@@ -31,7 +39,8 @@ class TestProposalService(BaseUnitTest):
             self.reportResult()
 
     def test_getProposalsByStatusOpen(self):
-        _proposals = self.getProposalService().GetProposalsByStatus('OPEN')
+        domainId = Domain.gql("WHERE title = :pTitle", pTitle=self._testDomainName).get().key().id()
+        _proposals = self.getProposalService().GetProposalsByDomainAndStatus(domainId, 'OPEN')
 
         if _proposals is not None:
             self.reportResult(message=' count='+str(_proposals.count()))
@@ -39,7 +48,8 @@ class TestProposalService(BaseUnitTest):
                 self.reportResult()
 
     def test_getProposalsByStatusClosed(self):
-        _proposals = self.getProposalService().GetProposalsByStatus('CLOSED')
+        domainId = Domain.gql("WHERE title = :pTitle", pTitle=self._testDomainName).get().key().id()
+        _proposals = self.getProposalService().GetProposalsByDomainAndStatus(domainId, 'CLOSED')
 
         if _proposals is not None:
             self.reportResult(message=' count='+str(_proposals.count()))
