eedef4209db0a171eb57e381afd4d6402cd07ef1
bretthop <brett.hoppz@gmail.com>
2013-02-12 07:30:50 +1100

    * Upgraded app to use WSGI handlers - This completes the migration to python 2.7

--- admin/AdminDispatcher.py
+++ admin/AdminDispatcher.py
@@ -1,15 +1,8 @@
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
 from admin.AdminController import AdminController
 
-def main():
-    app = webapp.WSGIApplication(
-        [
-            ('/doAdminAction', AdminController)
-        ],
-        debug=True)
-
-    run_wsgi_app(app)
-
-if __name__ == "__main__":
-    main()
+adminDispatcher = webapp.WSGIApplication(
+    [
+        ('/doAdminAction', AdminController)
+    ],
+    debug=True)
--- app/APIDispatcher.py
+++ app/APIDispatcher.py
@@ -1,5 +1,4 @@
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
 from app.resources.BackerVoteResource import BackerVoteResource
 from app.resources.DemoResources import DomainDemoResource, ProposalDemoResource
 
@@ -8,20 +7,14 @@ from app.resources.ProposalResource import *
 from app.resources.VoteResource import *
 from app.resources.LoginResource import *
 
-def main():
-    app = webapp.WSGIApplication(
-        [
-            ('/api/domains', DomainResource),
-            ('/api/proposals', ProposalResource),
-            ('/api/votes', VoteResource),
-            ('/api/backerVotes', BackerVoteResource),
-            ('/api/login', LoginResource),
-            ('/api/demo/domain', DomainDemoResource),
-            ('/api/demo/proposal', ProposalDemoResource)
-        ],
-        debug=True)
-
-    run_wsgi_app(app)
-
-if __name__ == "__main__":
-    main()
+apiDispatcher = webapp.WSGIApplication(
+    [
+        ('/api/domains', DomainResource),
+        ('/api/proposals', ProposalResource),
+        ('/api/votes', VoteResource),
+        ('/api/backerVotes', BackerVoteResource),
+        ('/api/login', LoginResource),
+        ('/api/demo/domain', DomainDemoResource),
+        ('/api/demo/proposal', ProposalDemoResource)
+    ],
+    debug=True)
